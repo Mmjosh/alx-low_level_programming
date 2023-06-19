1cc56c9cd27cd2d8b3571761f8833437c79d2f17
Mmjosh <morarajoshua36@gmail.com>
2023-06-19 12:11:37 -0700

    C program assign variable n a random num and identifies its last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-	printf("Last digit is %d and is 0\n", n % 10);
+	printf("Last digit of %d is %d and is 0\n", n % 10);
 	}
 	else if (n % 10 < 6 && n % 10 != 0)
 	{
