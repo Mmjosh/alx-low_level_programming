cba2652b89d3cca2d6592e8ddc373358982ddece
Mmjosh <morarajoshua36@gmail.com>
2023-06-21 17:58:31 -0700

    prints xtable from 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(32);
-					_putchar(32):
+					_putchar(32);
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
