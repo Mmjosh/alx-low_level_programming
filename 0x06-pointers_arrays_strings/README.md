cd1fd2ec85ff20a5f84618d8f01df249fdadffeb
Mmjosh <Mmjosh>
2023-06-30 03:19:18 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this repo will be about 0x06-pointers_arrays_strings;
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes a character to stdout
+ * @c: char to be printed
+ * Return: 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
