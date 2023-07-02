7166b885a3a55180c37c8ad9637d8269a3fc2cfe
Mmjosh <Mmjosh>
2023-07-02 07:11:28 -0700

    Fixed 1-main to avoid ∞ loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
+
+
+
+
+
+
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
-
