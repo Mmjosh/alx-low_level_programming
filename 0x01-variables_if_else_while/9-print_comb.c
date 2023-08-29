bae8c805c70d4a208f0cee2669154cd5051bd9b5
Mmjosh <morarajoshua36@gmail.com>
2023-08-29 04:00:37 -0700

    function displays all possible combos of single digit nums

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - prints all possible combinations of single digit numbers
  * Return: 0
  */
 int main(void)
 {
-	int num = 0;
+	int a = 0;
 
-	for (num = 0 ; num <= 9 ; num++)
+	while (a <= 9)
 	{
-	putchar(num + '0');
-
-	if (num != 9)
-	{
-	putchar(',');
-	putchar(' ');
+		putchar(a + '0');
+		if (a != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		a++;
 	}
-	}
-
 	putchar('\n');
-
 	return (0);
 }
