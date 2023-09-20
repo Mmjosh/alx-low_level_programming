ccee53fce8799c50f3b892052c0f473cf532291d
Mmjosh <morarajoshua36@gmail.com>
2023-09-20 08:12:51 +0300

    Function checks if char is a letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,27 @@
 #include "main.h"
+
 /**
- * _isalpha - checks for alphabets
- * @c: character to be checked
- *
- * Return: 1 if character is a letter, 0 otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: the character being checked
+ * Return: 1 if 'c' is a letter, otherwise return 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	int i;
+
+	/* '65 < x > 90'- ASCII Range for uppercase letters */
+	for (i = 65 ; i <= 90 ; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+
+	/* '97 < x > 122' - ASCII Range for lowercase characters */
+	for (i = 97 ; i <= 122 ; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+
+	return (0);
 }
