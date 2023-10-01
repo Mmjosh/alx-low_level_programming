0d7b303f515285e52d257978a513e70f1e0967b0
Mmjosh <morarajoshua36@gmail.com>
2023-10-01 13:16:04 +0300

    Program adds up positive No.s it receives as args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: pointer to an array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc < 1)
+	{
+		printf("%d\n", 0);
+	}
+
+	for (i = 1 ; i < argc ; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (isdigit(*argv[i]))
+		{
+			 sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
