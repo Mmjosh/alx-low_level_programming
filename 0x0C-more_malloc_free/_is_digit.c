f20c7a2ace53f1bc729938de279c8e5e6770e58d
Mmjosh <morarajoshua36@gmail.com>
2023-10-05 17:28:37 +0300

    Helper function

---
+++ 0x0C-more_malloc_free/_is_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_digit - checks if a character is a digit
+ * @c: the character to check
+ * Return: 1 if character is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
