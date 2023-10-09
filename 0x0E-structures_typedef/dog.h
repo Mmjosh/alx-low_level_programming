3de5a4aa97ce17762257c5e26a032973d1015158
Mmjosh <morarajoshua36@gmail.com>
2023-10-09 23:01:59 +0300

    Header file defines struct `dog`

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defines the qualities of a dog
+ * @name: name of the dog
+ * @age: its age
+ * @owner: whoever owns the dog
+ */
 struct dog
 {
 	char *name;
