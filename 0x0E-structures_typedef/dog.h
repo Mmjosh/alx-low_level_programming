27effd7535739e9c33d3b608b66e6778d07b7565
Mmjosh <morarajoshua36@gmail.com>
2023-10-10 09:07:45 +0300

    Created an alias,`dog_t` for the `struct dog`

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @age: its age
  * @owner: whoever owns the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
