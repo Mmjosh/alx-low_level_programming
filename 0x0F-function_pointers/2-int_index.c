abf3213861619d9137fc44c3aad468a6f6b7ad2d
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-02-03 18:00:48 +0300

    update `2-int_index.c-searches for an integer`

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,13 +16,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		exit(98);
+		return (-1);
 	else if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if ((*cmp)(array[i]) != 0)
+		if ((*cmp)(array[i]))
 			return (i);
 	}
 	return (-1);
