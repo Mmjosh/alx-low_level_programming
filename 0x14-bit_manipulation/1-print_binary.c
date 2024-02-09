816e8bed8cfd6cb752e98c2792da8d1b2896c1f1
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-02-09 21:01:13 +0300

    prints the binary rep. of a decimal

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: the decimal to be converted to binary.
+ * Return: void/nothing.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int u_v = 0;
+	unsigned int i_th = 0;
+	unsigned int lsb;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (n > 0)
+	{
+		lsb = n & 1;
+		u_v |= (lsb << i_th);
+		n >>= 1;
+		i_th++;
+	}
+
+	while (i_th > 0)
+	{
+		_putchar((u_v >> (i_th - 1)) & 1 ? '1' : '0');
+		i_th--;
+	}
+}
