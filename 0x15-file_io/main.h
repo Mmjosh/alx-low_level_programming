81843c3c1e6b4b3d51afb682dcb0e43453c94241
Mmjosh <132352996+Mmjosh@users.noreply.github.com>
2024-02-16 23:14:02 +0300

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,8 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_98(int fd_to, char *buffer, char *argv);
+void error_99(int fd_to, char *buffer, char *argv);
+void error_100(int fd_t0, char *buffer);
 
 #endif
